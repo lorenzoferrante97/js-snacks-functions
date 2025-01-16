747ecb597a811d05340f5ffe7c4b996159aef913
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-16 11:48:26 +0100

    start: start es. 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,3 +1,5 @@
+//start es. 2
+
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
