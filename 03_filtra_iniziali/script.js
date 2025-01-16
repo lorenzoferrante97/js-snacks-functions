76eaf0fcb3ca525304cfb55e4eb7956a5093a306
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-16 12:03:44 +0100

    start: start es. 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,3 +1,5 @@
+// start es. 3
+
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
