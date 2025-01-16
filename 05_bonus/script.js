7f1bcdc10e4c859d5a82c4e9fe30a94267ce8895
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-16 12:49:44 +0100

    end: end es. bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -14,12 +14,21 @@ let ora = 18;
 // Dichiara la funzione qui.
 
 let salutoOrario = (nome) => {
-    
+
+    if ((ora >= 5) && (ora <= 13)) {
+        return "Buongiorno";
+    } else if ((ora >= 14) && (ora <= 17)) {
+        return "Buon pomeriggio";
+    } else if ((ora >= 18) && (ora <= 23)) {
+        return "Buonasera";
+    } else if ((ora == 0) || ((ora >= 1) && (ora <= 4))) {
+        return "Buonanotte";
+    }
 }
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(salutoOrario(name), ` ${name}`)
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
