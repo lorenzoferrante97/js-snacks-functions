ec1fafb1e2c127c45ebab989014aca1a6634c353
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-16 12:28:24 +0100

    end: end es. 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -8,16 +8,27 @@ let vocaliEstratte = [];
 
 // Dichiara la funzione qui.
 
-function estraiVocali (par, count, res) {
+function estraiVocali (par, res) {
 
     for (let i = 0; i < par.length; i++) {
-        
+        if ((par.charAt(i) == "a") ||
+        (par.charAt(i) == "e") ||
+        (par.charAt(i) == "i") ||
+        (par.charAt(i) == "o") ||
+        (par.charAt(i) == "u"))
+        {
+            counterVocali++;
+            res.push(par[i]);
+        }
     }
+
+    return res;
 }
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+estraiVocali(word, vocaliEstratte);
+console.log(`${counterVocali} `, vocaliEstratte.join(","))
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
