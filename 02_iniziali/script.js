713a7ec6c0c9ae504ce496093819b88491e25196
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-16 11:56:30 +0100

    add: array iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -3,7 +3,7 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+let iniziali = [];
 
 // Dichiara la funzione qui.
 
