a1c04d5e9856ae9982de6a9a210c916707312c96
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-16 12:19:32 +0100

    add: function + variables

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -3,9 +3,14 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
-
+let counterVocali = 0;
+let vocaliEstratte = [];
 
 // Dichiara la funzione qui.
+
+function estraiVocali (par, count, res) {
+
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
