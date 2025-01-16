a2327199a93f013b4622138ac69edc5d1b990f56
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-16 12:08:13 +0100

    add: function + variables

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -3,9 +3,14 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+let paroleFinali = [];
+let lettera = "A";
 
 // Dichiara la funzione qui.
+
+function matchFirstLetter (arr, lett, res) {
+    
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
